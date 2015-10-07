193d7c64669886b08509fe44c905d5a61bcf92cc
Zakharov Alexey <alexey.zakharov@altoros.com>
2015-10-07 13:47:45 +0300

    Uploaded config

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+/usr/lib/courier/courier-authlib/authdaemond &
+/sbin/rpcbind -w &
+/usr/sbin/famd -T 0
+/usr/sbin/couriertcpd -address=0 -maxprocs=40 -maxperip=20 -nodnslookup -noidentlookup 143 /usr/lib/courier/courier/imaplogin /usr/bin/imapd Maildir &
+/usr/sbin/couriertcpd -maxprocs=40 -maxperip=4 -nodnslookup -noidentlookup -address=0 110 /usr/lib/courier/courier/courierpop3login /usr/lib/courier/courier/courierpop3d Maildir 
