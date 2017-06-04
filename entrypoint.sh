40529fb93e51d636f9b812cf422ec6a7ca03354f
Alexey Zakharov <baber@mosga.net>
2017-06-04 17:15:12 +0300

    Replaced stdout with '-stderr' key for logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 /usr/lib/courier/courier-authlib/authdaemond &
 /sbin/rpcbind -w &
 /usr/sbin/famd -T 0
-/usr/sbin/couriertcpd -address=0 -maxprocs=40 -maxperip=20 -nodnslookup -noidentlookup 143 /usr/lib/courier/courier/imaplogin /usr/bin/imapd Maildir >/var/log/courier/mail.log &
-/usr/sbin/couriertcpd -maxprocs=40 -maxperip=4 -nodnslookup -noidentlookup -address=0 110 /usr/lib/courier/courier/courierpop3login /usr/lib/courier/courier/courierpop3d Maildir >/var/log/courier/mail.log 
+/usr/sbin/couriertcpd -address=0 -maxprocs=40 -maxperip=20 -nodnslookup -noidentlookup -stderr=/var/log/courier/mail.log 143 /usr/lib/courier/courier/imaplogin /usr/bin/imapd Maildir &
+/usr/sbin/couriertcpd -maxprocs=40 -maxperip=4 -nodnslookup -noidentlookup -address=0 -stderr=/var/log/courier/mail.log 110 /usr/lib/courier/courier/courierpop3login /usr/lib/courier/courier/courierpop3d Maildir 
